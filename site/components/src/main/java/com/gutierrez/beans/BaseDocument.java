88c1c4eea48e5130ef20d19a2b5a086e7a458cc1
Pedro <pgutierrez@morneaushepell.com>
2022-03-15 10:20:00 +0000

    starting point

---
+++ site/components/src/main/java/com/gutierrez/beans/BaseDocument.java
@@ -0,0 +1,9 @@
+package com.gutierrez.beans;
+
+import org.hippoecm.hst.content.beans.Node;
+import org.hippoecm.hst.content.beans.standard.HippoDocument;
+
+@Node(jcrType="petercms:basedocument")
+public class BaseDocument extends HippoDocument {
+
+}
